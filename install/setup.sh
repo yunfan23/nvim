2842c89213241a5f2820216b29cb43a98c82c33e
Yunfan Zhang <yunfan001@e.ntu.edu.sg>
2020-10-23 23:10:10 +0800

    pynvim

--- install/setup.sh
+++ install/setup.sh
@@ -1,3 +1,4 @@
 curl -fLo $HOME/nvim/nvim.appimage --create-dirs https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+pip install pynvim
 brew install --HEAD neovim
 brew install luarocks
