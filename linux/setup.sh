e73aa8c5fb3415207cd78d63a60893352034aa5c
Yunfan Zhang <yunfan001@e.ntu.edu.sg>
2020-11-12 16:31:08 +0800

    vim

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,7 +1,4 @@
-curl -fLo $HOME/nvim/nvim.appimage --create-dirs https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-pip install pynvim
-brew install --HEAD neovim
-brew install luarocks
-brew install ack
-pip install grip
-pip install mdv
+#!/usr/bin/env bash
+export DIR_PWD="" 
+export PYTHONPATH="\YTHONPATH:\IR_PWD"
+echo \\YTHONPATH
